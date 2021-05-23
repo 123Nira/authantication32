4cbfee0564f67ca57c67270c5b0dcb87f0c54941
Niraj kumar <bhanu8759400@gmail.com>
2021-05-23 15:50:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import axios from 'axios'
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
 
 function App() {
+  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } = useAuth0()
+
+function callApi(){
+axios.get("http://localhost:3001/").then(response=>console.log(response.data))
+.catch(error=>console.log(error.message))
+}
+function callProtectedApi(){
+  axios.get("http://localhost:3001/protected").then(response=>console.log(response.data))
+  .catch(error=>console.log(error.message))
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Authenticated</h1>
+      <ul>
+        <li>
+          <button onClick={ loginWithPopup }>Login with Popup</button>
+        </li><br />
+        <li>
+          <button onClick={ loginWithRedirect }>Login with Redirect</button>
+        </li><br />
+        <li>
+          <button onClick={ logout }>Logout</button>
+        </li><br />
+
+      </ul>
+      <h3>User is { isAuthenticated ? "Logged in" : "Not Logged in" }</h3>
+
+      <ul>
+        <li>
+          <button onClick={callApi}>Call API rout </button>
+        </li><br />
+        <li>
+          <button onClick={callProtectedApi}>Call Protected API rout</button>
+        </li>
+      </ul>
+      <pre style={ { textAlign: 'start' } }>{ JSON.stringify(user, null, 2) }</pre>
     </div>
   );
 }
